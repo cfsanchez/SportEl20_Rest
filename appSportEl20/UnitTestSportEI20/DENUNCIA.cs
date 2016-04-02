db20fed28310bb642f3425b646eb5908fe287970
cfsanchez <cfsanchez.a@outlook.com>
2016-04-01 22:37:19 -0500

    Clases test

---
+++ appSportEl20/UnitTestSportEI20/DENUNCIA.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSportEI20
+{
+   public class Denuncia
+    {
+        public int ID_DENUNCIA { get; set; }
+        
+        public int ID_USUARIO { get; set; }
+        
+        public string TIPODENUNCIA { get; set; }
+        
+        public DateTime FECHADENUNCIA { get; set; }
+        
+        public string DESCRIPCION { get; set; }
+        
+        public string ESTADO { get; set; }
+
+        
+        public List<DETALLE_FOTO> DETALLE_FOTOS { get; set; } 
+    }
+}
---
+++ appSportEl20/UnitTestSportEI20/DETALLE_FOTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestSportEI20
+{
+    public class DETALLE_FOTO
+    {
+
+        public int ID_DETALLE_DENUNCIA { get; set; }
+
+        public int ID_DENUNCIA { get; set; }
+
+        public string FOTODENUNCIA { get; set; }
+    }
+}
